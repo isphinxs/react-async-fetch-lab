2035812be3e4ef0c223f11ace380f7fd5605663c
Samantha Ostrowski <isphinxs@hotmail.com>
2021-07-31 15:22:28 -0400

    commit tree 515c8f52084e4fc176e1ba40ac5fa10c906224be

--- src/App.js
+++ src/App.js
@@ -1,1 +1,31 @@
 // create your App component here
+
+import React, {Component} from "react";
+
+class App extends Component {
+    state = {
+        astronauts: []
+    }
+
+    componentDidMount() {
+        fetch("http://api.open-notify.org/astros.json")
+            .then(resp => resp.json())
+            .then(astronauts => {
+                // debugger;
+                this.setState({ astronauts: astronauts.people });
+            })
+    }
+
+    render() {
+        return(
+            <div>
+                <h1>Astronauts in Space</h1>
+                <ul>
+                    {this.state.astronauts.map(astronaut => <li>{astronaut.name}</li>)}
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default App;
